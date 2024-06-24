2536825d1efc33990a251b7a183ac177ffcb8fc5
𝐃𝐇𝐈𝐑𝐀𝐉 <mauryadheeraj226@gmail.com>
2024-06-24 15:35:55 +0530

    Update start.py

--- FLASH/modules/start.py
+++ FLASH/modules/start.py
@@ -1,6 +1,6 @@
 #MIT License
 
-#Copyright (c) 2024 ᴋᴜɴᴀʟ [AFK]
+#Copyright (c) 2024 ᴅʜɪʀᴀᴊ [AFK]
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -25,8 +25,8 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_OP = [
     [
-        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/Kexx_XD"),
-        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/STORM_CHATZ"),
+        Button.url("ᴏᴡɴᴇʀ 🥀", "https://t.me/II_DHIRAJ_II"),
+        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/FLASH_Chatz"),
     ],
     [
         Button.url(
@@ -34,8 +34,8 @@ START_OP = [
         ),
     ],
     [
-        Button.url("ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ❄️", "https://github.com/VARC9210/STORM"),
-        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/STORM_TECHH"),
+        Button.url("ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ❄️", "https://github.com/II_DHIRAJ_II/flashXspam"),
+        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/Flash_TECHH"),
     ],
 ]
 
@@ -55,8 +55,8 @@ async def start(event):
         bot_name = KEX.first_name
         bot_id = KEX.id
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}]\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n➖➖➖➖➖➖➖➖➖➖➖\n"
-        TEXT += f"» **ꜱᴇɴꜱᴇɪ : [⏤͟͞〲ᴋᴇx](https://t.me/kexx_xd)**\n"
-        TEXT += f"» **ꜱᴛᴏʀᴍ :** `M3.0` \n"
+        TEXT += f"» **ꜱᴇɴꜱᴇɪ : [⏤͟͞〲ᴅʜɪʀᴀᴊ](https://t.me/kexx_xd)**\n"
+        TEXT += f"» **ғʟᴀsʜ :** `M3.0` \n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ :** `3.11` \n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ :** `{__version__}`\n➖➖➖➖➖➖➖➖➖➖➖"        
         await event.client.send_file(
