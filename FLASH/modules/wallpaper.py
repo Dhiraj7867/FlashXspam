629a170f633d25b1bc726e134cf1bfbf3b5e799a
𝐃𝐇𝐈𝐑𝐀𝐉 <mauryadheeraj226@gmail.com>
2024-07-05 18:59:05 +0530

    Update wallpaper.py

--- FLASH/modules/wallpaper.py
+++ FLASH/modules/wallpaper.py
@@ -25,7 +25,7 @@ import random
 from telethon import events
 from telethon import events, functions, types
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
-from FLASH.data import WALLPAPER
+from FLASH.data import WALLDATA
 from FLASH.data import DEV
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%swspam(?: |$)(.*)" % hl))
